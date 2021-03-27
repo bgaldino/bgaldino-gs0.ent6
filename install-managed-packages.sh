57e0e902a970a7c37b5273a9b55e313e1e9d86e2
Brian Galdino <bgaldino@salesforce.com>
2021-03-26 23:07:27 -0700

    Update CPQ to 230.6

--- install-managed-packages.sh
+++ install-managed-packages.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Comment out the packages that you don't want to install in your org
 
-# Salesforce CPQ 230.5 (SBQQ)
-sfdx force:package:install -p 04t4N000000szMmQAI
+# Salesforce CPQ 230.6 (SBQQ)
+sfdx force:package:install -p 04t4N000000szN1QAI
 
 # Salesforce Billing (230.2) (blng)
 sfdx force:package:install -p 04t0K000001CpjYQAS
 
-# Salesforce Advanced Approvals (230.0)
-sfdx force:package:install -p 04t4W000001OiBQQA0
+# Salesforce Advanced Approvals (230.1)
+sfdx force:package:install -p 04t4W000002LinGQAS
 
 # Mock Payment Adapter
 sfdx force:package:install -p 04t2x000003t1MY
